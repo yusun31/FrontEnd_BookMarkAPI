b9fa52b50061c896327dd1b378be075d6ac74150
yssong1 <ys.song@ilts.co.kr>
2024-05-21 13:44:43 +0900

    Link component add

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
 
 const NavBar = () => (
   <header>
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand">Bookmarker</a>
+        <Link href="/" className="navbar-brand">
+            Bookmarker
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,7 +23,9 @@ const NavBar = () => (
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link">Add Bookmark</a>
+              <Link href="/add" className="nav-link">
+                Add Bookmark
+              </Link>     
             </li>
           </ul>
         </div>
