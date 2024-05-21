5c9d8ff8830ffe5ae9548c927d554b3ca7f94aa8
yssong1 <ys.song@ilts.co.kr>
2024-05-21 15:08:49 +0900

    Bookmark component add

--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -1,5 +1,6 @@
 import { BookmarkType } from '@/types';
 import React from 'react';
+import Bookmark from '@/app/components/Bookmark';
 
 type Props = {
     bookmarks: BookmarkType[]
@@ -10,11 +11,12 @@ const Bookmarks: React.FC<Props> = ({bookmarks}) => {
         <div>
             <h1>Welcome to Bookmarker</h1>
             {
-            bookmarks.map(bookmark => {
-                return (
-                    <h2 key={bookmark.id}>{bookmark.title}</h2>
-                )
-            })
+            bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
+            // bookmarks.map(bookmark => {
+            //     return (
+            //         <h2 key={bookmark.id}>{bookmark.title}</h2>
+            //     )
+            // })
             }            
         </div>
     );
