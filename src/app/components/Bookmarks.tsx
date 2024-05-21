039cfec2482b285e589f9a652f2491848e9acfbf
yssong1 <ys.song@ilts.co.kr>
2024-05-21 15:37:52 +0900

    Bookmark Pagination add

--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -2,6 +2,7 @@
 import { BookmarksResponse } from "@/types";
 import React from 'react';
 import Bookmark from '@/app/components/Bookmark';
+import Pagination from '@/app/components/Pagination';
 
 type Props = {
     // bookmarks: BookmarkType[]
@@ -12,6 +13,7 @@ const Bookmarks: React.FC<Props> = ({bookmarks}) => {
     return (
         <div>
             <h1>Welcome to Bookmarker</h1>
+            <Pagination bookmarks={bookmarks} />
             {
             bookmarks.data.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
             //bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
